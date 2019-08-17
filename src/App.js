775e6644107b0ba51c2631dd93f1677c93a212a1
Gemma Barrera <lagemyo@gmail.com>
2019-08-17 16:27:49 +0200

    finished folder and files structure

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Home from './pages/HomePage';
+import About from './pages/AboutPage';
+import Products from './pages/ProductsPage';
+import Contact from './pages/ContactPage';
+import SingleProduct from './pages/SingleProductPage';
+import Default from './pages/Default';
+
 class App extends Component {
   render() {
     return (
-      <h1>hello from tech store</h1>
+      <h1>hello from tech store
+
+      </h1>
     );
   }
 }
---
+++ src/pages/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function HomePage() {
+    return (
+        <>
+            <h1>hello From HomePage</h1>
+        </>
+    );
+}
