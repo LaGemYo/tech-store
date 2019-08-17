0d6c2e3f1950a29fff41823c6e69d67d2efcb8a2
Gemma Barrera <lagemyo@gmail.com>
2019-08-17 17:40:46 +0200

    added pages files and prepared router

--- src/App.js
+++ src/App.js
@@ -7,14 +7,27 @@ import About from './pages/AboutPage';
 import Products from './pages/ProductsPage';
 import Contact from './pages/ContactPage';
 import SingleProduct from './pages/SingleProductPage';
+import Cart from './pages/CartPage';
 import Default from './pages/Default';
+import { runInContext } from 'vm';
+
+import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <h1>hello from tech store
-
-      </h1>
+      <>
+        {/*navbar, sidebar, cart, footer */}
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/products" exact component={Products} />
+          <Route path="/product/:id" component={SingleProduct} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/default" component={Default} />
+        </Switch>
+      </>
     );
   }
 }
