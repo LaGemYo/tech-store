31292dca6e5ef6977bd5ff737acf5ebbd7c8642f
Gemma Barrera <lagemyo@gmail.com>
2019-08-18 13:48:33 +0200

    added featured product images

--- src/components/HomePage/Featured.js
+++ src/components/HomePage/Featured.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import Product from '../Product';
+import Title from '../Title';
+import {Link} from 'react-router-dom';
+import {ProductConsumer} from '../../context';
 
 export default function Featured() {
     return (
-        <div>Hello from featured</div>
+        <section className="py-5">
+            <div className="container">
+                {/* Title */}
+                <Title title="featured products" center="true" />
+                {/* products */}
+                <div className="row">
+                    <ProductConsumer>
+                        {value => {
+                            const {featuredProducts} = value;
+                            return featuredProducts.map(product => (
+                            <Product 
+                            key={product.id}
+                            product={product}
+                            >
+                            </Product>))
+                        }}
+                    </ProductConsumer>
+                </div>
+            </div>
+        </section>
     )
 }
---
+++ src/components/Product.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Styled from 'styled-components';
+import {Link} from 'react-router-dom';
+import {FaSearch, FaCartPlus} from 'react-icons/fa';
+import {ProductConsumer} from '../context';
+
+export default function Product({product}) {
+    return (
+        <ProductConsumer>
+            {value => {
+                const {addToCart, setSingleProduct} = value;
+                return (
+                    <ProductWrapper 
+                    className="col-10 mx-auto col-sm-8 col-md-6 col-lg-4 my-3"
+                    >
+                        <div className="card">
+                            <div className="img-container">
+                                <img src={product.image}
+                                 alt="product"
+                                 clasName="card-img-top p-5"
+                                 style={{height:"300px"}}
+                                 />
+                                 <div className="product-icon">
+                                     <Link 
+                                     to={`/products/${product.id}`} 
+                                     onClick={() => {setSingleProduct(product.id)}}
+                                     >
+                                         <FaSearch className="icon" />
+                                     </Link>
+                                     <FaCartPlus 
+                                     className="icon"
+                                     onClick={() => {addToCart(product.id)}}
+                                    />
+                                 </div>
+                            </div>
+                        </div>
+                    </ProductWrapper>
+                )
+            }}
+        </ProductConsumer>
+    )
+}
+
+const ProductWrapper = Styled.div`
+
+`;
