a242141748d0165620a1f3b756841fe955b9ce7f
Gemma Barrera <lagemyo@gmail.com>
2019-08-21 23:41:41 +0200

    fixed distribution of the elements in the singleProduct page

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Hero from '../components/Hero';
 import singleProductImg from '../images/singleProductBcg.jpeg';
-import {ProductConsumer} from '../context';
+import { ProductConsumer } from '../context';
 
 export default function SingleProductPage() {
     return (
@@ -10,36 +10,53 @@ export default function SingleProductPage() {
             <Hero img={singleProductImg} title="single product" />
             <ProductConsumer>
                 {value => {
-                    const {singleProduct, addToCart, loading} = value;
+                    const { singleProduct, addToCart, loading } = value;
 
-                    if(loading) {
+                    if (loading) {
                         console.log('Hello from loading');
                         return <h1>product loading...</h1>
                     }
                     const {
-                        company, 
+                        company,
                         description,
-                        id, 
-                        price, 
-                        title, 
+                        id,
+                        price,
+                        title,
                         image
                     } = singleProduct
                     return <section className="py-5">
                         <div className="container">
                             <div className="row">
-                                <div className= "col-10 mx-auto col-sm-8 col-md-6 my-3">
-                                    <img src={`../${image}`} 
-                                    alt="single product" 
-                                    className="img-fluid" 
+                                <div className="col-10 mx-auto col-sm-8 col-md-6 my-3">
+                                    <img src={`../${image}`}
+                                    alt="single product"
+                                    className="img-fluid"
                                     />
-                            <div className= "col-10 mx-auto col-sm-8 col-md-6 my-3">
-                                <h5 className="text-title mb-4">
-                                    model : {title}
-                                </h5>
-                                <h5 className="text-capitalize text-muted mb-4">
-                                    company: {company}
-                                </h5>
-                            </div>
+                                </div>
+                                <div className="col-10 mx-auto col-sm-8 col-md-6 my-3">
+                                    <h5 className="text-title mb-4">
+                                        model : {title}
+                                    </h5>
+                                    <h5 className="text-capitalize text-muted mb-4">
+                                        company: {company}
+                                    </h5>
+                                    <h5 className="text-capitalize text-main mb-4">
+                                        price: ${price}
+                                    </h5>
+                                    <p className="text-capitalize text-title mt-3">
+                                        some info about products :
+                                    </p>
+                                    <p>{description}</p>
+                                    <button type="button"
+                                    className="main-link"
+                                    style={{ margin: '0.75rem' }}
+                                    onClick={() => addToCart(id)}
+                                    >
+                                        add to cart
+                                </button>
+                                    <Link to='/products' className="main-link" style={{ margin: '0.75rem' }}>
+                                        back to products
+                                    </Link>
                                 </div>
                             </div>
                         </div>
