402adcb915df90dffb6c9da5f28412d586a4ab57
Gemma Barrera <lagemyo@gmail.com>
2019-08-21 16:29:32 +0200

    started painting singleProduct page

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import Hero from '../components/Hero';
+import singleProductImg from '../images/singleProductBcg.jpeg';
+import {ProductConsumer} from '../context';
 
 export default function SingleProductPage() {
     return (
         <>
-            <h1>hello From SingleProduct Page</h1>
+            <Hero img={singleProductImg} title="single product" />
+            <ProductConsumer>
+                {value => {
+                    const {singleProduct, addToCart, loading} = value;
+
+                    if(loading) {
+                        console.log('Hello from loading');
+                        return <h1>product loading...</h1>
+                    }
+                    const {
+                        company, 
+                        description,
+                        id, 
+                        price, 
+                        title, 
+                        image
+                    } = singleProduct
+                    return <section className="py-5">
+                        <div className="container">
+                            <div className="row">
+                                <div className= "col-10 mx-auto col-sm-8 col-md-6 my-3">
+                                    <img src={`../${image}`} 
+                                    alt="single product" 
+                                    className="img-fluid" 
+                                    />
+                            <div className= "col-10 mx-auto col-sm-8 col-md-6 my-3">
+                                <h5 className="text-title mb-4">
+                                    model : {title}
+                                </h5>
+                                <h5 className="text-capitalize text-muted mb-4">
+                                    company: {company}
+                                </h5>
+                            </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                }}
+            </ProductConsumer>
         </>
     );
 }
